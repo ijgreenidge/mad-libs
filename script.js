2146b0573ef7fb208a7b7d85952b4f47bb27353e
ijgreenidge <igreenidge2018@bklawtech.com>
2017-01-10 22:54:55 +0000

    mad-libs

---
+++ script.js
@@ -0,0 +1,16 @@
+var words = {
+    animals : "bears",
+    thing1 : "books",
+    feeling : "depressed",
+    professional : "rapper",
+    clothes : "hoodie",
+    thing2 : "laptops",
+    person : "Ashley",
+    place : "Narnia",
+    verb : "flying",
+    food : "hambugers",
+    
+};
+
+var story = "Say" + " " + words.food + " " + "the photographer said as the camera flashed!" + " " + words.person + " " + "and I had gone to" + " " + words.place + " " + "to get our photos taken today. The first photo we really wanted was a picture of us dressed as" + " " + words.animals + " " + "pretending to be a " + " " + words.professional+ " " + "When we saw the proofs of it, I was a bit" + " " + words.feeling + " " + "because it looked different than I had imagined it would. (I hadn’t imagined so many" + " " + words.thing1 + " " + "behind us.) However, the second photo was exactly what I wanted. We both looked like" + " " + words.thing2 + " " + "wearing" + " " + words.clothes + " " + "and" + " " + words.verb + " " + ", which was exactly what I had in mind!" 
+console.log(story);
